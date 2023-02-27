59584e1abf631ee6753e52bac0a7addb3da5d9fb
noceive <nocivescience@gmail.com>
2023-02-27 00:36:54 -0300

    mi primer commit

---
+++ scrip.js
@@ -0,0 +1,34 @@
+const svg=document.querySelector('svg')
+const curve=document.getElementById('curve')
+const controlPoint=document.getElementById('control-point')
+const posXLabel=document.querySelector('.posX')
+const posYLabel=document.querySelector('.posY')
+const svgViewBoxWidth=190
+let isMouseDown=false
+let point={
+    x:95,
+    y:80
+}
+function updateCurve(x,y){
+    let svgRect=svg.getBoundingClientRect()
+    let scale=svgRect.width/svgViewBoxWidth
+    let width=(svgRect.width/scale)-5
+    let height=(svgRect.height/scale)-6
+    if(x&&y){
+        point.x=Math.ceil((x-svgRect.x)/scale)
+        point.y=Math.ceil((y-svgRect.y)/scale)
+    }
+    point.x=point.x<5?5:point.x
+    point.y=point.y<5?5:point.y
+    point.x=point.x>width?Math.ceil(width):point.x
+    point.y=point.y>height?Math.ceil(height):point.y
+    posXLabel.textContent=point.x.toFixed(2)
+    posYLabel.textContent=point.y.toFixed(2)
+    controlPoint.setAttribute('cx',point.x)
+    controlPoint.setAttribute('cy',point.y)
+    curve.setAttribute('d',curve.getAttribute('d').replace(/Q (\d+(\.\d+)?) (\d+(\.\d+)?)/,`Q ${point.x} ${point.y}`))
+}
+function onMouseDown(e){
+    updateCurve(e.clientX,e.clientY)
+}
+document.addEventListener('mousedown',onMouseDown)
